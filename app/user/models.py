02384a08382cb0fba944f6f3a26f2c596d294fe1
LaberryO <laberryo0912@gmail.com>
2026-09-16 16:10:09 +0900

    feat: User Entity

---
+++ app/user/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String
+from pydantic import BaseModel, ConfigDict
+
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(30), nullable=False)
+
+class UserCreateRequest(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+
+    model_config = ConfigDict(from_attributes=True)
