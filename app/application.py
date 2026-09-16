42a4712012110aad7063aa821e214608ea6f5ae2
LaberryO <laberryo0912@gmail.com>
2026-09-16 16:11:55 +0900

    feat: ServerApplicatiion

---
+++ app/application.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Request
+from starlette.responses import JSONResponse
+
+from app.database import Base, engine
+from app.user.controller import router as user_router
+
+from .settings import settings
+
+class ServerApplication(FastAPI):
+    def __init__(self, title="SJJEONG-DEV-SERVER", version="1.0.0", **kwargs):
+        super().__init__(title=title, version=version, **kwargs)
+
+        self._init_database()
+        self._init_routers()
+
+    def _init_database(self):
+        Base.metadata.create_all(bind=engine)
+
+    def _init_routers(self):
+        self.include_router(user_router)
+
+    def _init_middleware(self):
+        @self.middleware("http")
+        async def ip_whitelist_for_docs(request: Request, call_next):
+            if request.url.path in ["/docs", "/openapi.json", "/redoc"]:
+                client_ip = request.client.host
+                if client_ip != settings.dev_ip and client_ip != "127.0.0.1":
+                    return JSONResponse(
+                        status_code=403,
+                        content={"detail": "Forbidden: Not-allowed IP"}
+                    )
+            return await call_next(request)
